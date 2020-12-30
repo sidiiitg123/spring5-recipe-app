d9d869d3551f3f6acb81b6f5551a493819ae49bf
sidiiitg123 <sudhanshuupadhyay457@gmail.com>
2020-12-30 20:06:31 +0530

    Added Closing #14

---
+++ src/test/java/com/springframework/spring5recipeapp/Repository/UnitOfMeasureRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.springframework.spring5recipeapp.Repository;
+
+import com.springframework.spring5recipeapp.model.UnitOfMeasure;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryTest {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void findByDescription() {
+        Optional<UnitOfMeasure> uomOptional=unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon",uomOptional.get().getDescription());
+    }
+}
---
+++ src/test/java/com/springframework/spring5recipeapp/controller/RecipeControllerTest.java
@@ -0,0 +1,52 @@
+package com.springframework.spring5recipeapp.controller;
+
+import com.springframework.spring5recipeapp.model.Recipe;
+import com.springframework.spring5recipeapp.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.lang.reflect.Method;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+public class RecipeControllerTest {
+    @Mock
+    RecipeService recipeService;
+
+    RecipeController controller;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        controller = new RecipeController(recipeService);
+    }
+
+    @Test
+    public void testGetRecipe() throws Exception {
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        when(recipeService.findById(anyLong())).thenReturn(recipe);
+
+        mockMvc.perform(get("/recipe/show/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/show"));
+    }
+}
