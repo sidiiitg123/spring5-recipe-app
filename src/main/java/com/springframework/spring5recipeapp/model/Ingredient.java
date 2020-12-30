105c5faf2d0b2b9e6d0f86710cfe9385266c6e48
sidiiitg123 <sudhanshuupadhyay457@gmail.com>
2020-12-30 22:42:44 +0530

    Added Closing #15

--- src/main/java/com/springframework/spring5recipeapp/model/Ingredient.java
+++ src/main/java/com/springframework/spring5recipeapp/model/Ingredient.java
@@ -25,10 +25,16 @@ public class Ingredient {
     public Ingredient() {
     }
 
-    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
+//    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
+//        this.description = description;
+//        this.amount = amount;
+//        this.uom = uom;
+//    }
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
         this.description = description;
         this.amount = amount;
         this.uom = uom;
+        this.recipe = recipe;
     }
 
 }
