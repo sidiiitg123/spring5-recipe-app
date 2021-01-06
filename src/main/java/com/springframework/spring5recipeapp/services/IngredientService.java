6bbe71cfd588f892ea389fdac432dc9b8c6801b8
sidiiitg123 <sudhanshuupadhyay457@gmail.com>
2021-01-06 22:18:33 +0530

    Closing #23

--- src/main/java/com/springframework/spring5recipeapp/services/IngredientService.java
+++ src/main/java/com/springframework/spring5recipeapp/services/IngredientService.java
@@ -5,5 +5,6 @@ import com.springframework.spring5recipeapp.commands.IngredientCommand;
 public interface IngredientService {
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
     IngredientCommand saveIngredientCommand(IngredientCommand command);
+    void deleteById(Long recipeId, Long idToDelete);
 
 }
